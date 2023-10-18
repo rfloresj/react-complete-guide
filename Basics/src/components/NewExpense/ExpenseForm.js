d2e3d548fc2013f0fdfb0b763c1e137dd782fb0e
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-18 15:26:07 -0600

    Alternative:Creating A Shared Handler Function

--- Basics/src/components/NewExpense/ExpenseForm.js
+++ Basics/src/components/NewExpense/ExpenseForm.js
@@ -37,21 +37,31 @@ const ExpenseForm = () => {
     // })
   };
 
+  const inputChangeHandler = (identifier, value) => {
+    if(identifier === 'title') {
+      setEnteredTitle(value);
+    } else if (identifier === 'date'){
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
+  };
+
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" onChange={(event) => inputChangeHandler('title', event.target.value)} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" onChange={(event) => inputChangeHandler('amount', event.target.value)} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+          <input type="date" min="2019-01-01" max="2023-12-31" onChange={(event) => inputChangeHandler('date', event.target.value)} />
         </div>
       </div>
       <div className="new-expense__actions">
