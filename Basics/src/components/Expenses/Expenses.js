6074ad1593c07322293a2f97050926da59c49d2b
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-27 21:18:59 -0600

    End-React-State-&-Working-W/Events

--- Basics/src/components/Expenses/Expenses.js
+++ Basics/src/components/Expenses/Expenses.js
@@ -8,16 +8,6 @@ import ExpensesFilter from "./ExpensesFilter";
 const NewExpenseItem = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  let filterInfoText = "2019, 2021 & 2022";
-
-  if (filteredYear === "2019") {
-    filterInfoText = "2020, 2021 & 2022";
-  } else if (filteredYear === "2021") {
-    filterInfoText = "2019, 2020 & 2022";
-  } else {
-    filterInfoText = "2019, 2020 & 2021";
-  }
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -29,7 +19,6 @@ const NewExpenseItem = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <p>Data for years {filterInfoText} is hidden.</p>
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
